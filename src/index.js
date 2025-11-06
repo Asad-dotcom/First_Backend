6c6cb5246f83e31a23820be386fae29fa518fb93
Asad-dotcom <asad209673@gmail.com>
2025-11-07 00:46:36 +0500

    api test using postman

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-// import mongoose from "mongoose";
-// import  { DB_NAME } from './constants'
-
 import dotenv from "dotenv"
-import express from "express";
-const app = express();
-
+import { app } from "./app.js"
 import { connectDB } from "./db/index.js"
+
 dotenv.config({
     path : "./.env"
 })
 
-
-
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`App is Listening on ${process.env.PORT}`)
+        console.log(`App is Listening on port ${process.env.PORT || 8000}`)
     })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!", err);
 })
 .catch((err) =>{
     console.log("DB not Connected", err)
